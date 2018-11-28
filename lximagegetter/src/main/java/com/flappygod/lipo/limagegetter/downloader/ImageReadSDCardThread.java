cb1ae85c9c04081a21b3c2347d293d49f19e4fad
lijunlin <327603258@qq.com>
2018-11-28 15:49:50 +0800

    修改代码，去掉重复无用代码

--- lximagegetter/src/main/java/com/flappygod/lipo/limagegetter/downloader/ImageReadSDCardThread.java
+++ lximagegetter/src/main/java/com/flappygod/lipo/limagegetter/downloader/ImageReadSDCardThread.java
@@ -12,6 +12,7 @@ import com.flappygod.lipo.limagegetter.callback.LXReadSDCardCallback;
 import com.flappygod.lipo.limagegetter.option.LXImageReadOption;
 import com.flappygod.lipo.limagegetter.tools.ImageReadTool;
 import com.flappygod.lipo.limagegetter.tools.BitMapCache;
+import com.flappygod.lipo.limagegetter.tools.NameTool;
 
 public class ImageReadSDCardThread extends Thread {
 
@@ -139,71 +140,42 @@ public class ImageReadSDCardThread extends Thread {
 
 		// 取得这个被保存的文件名
 		String downImagePath = path;
-
-		// 首先查看SD卡中是否存在这个文件
-		if (ImageReadTool.isFileExsitsAntNotDic(downImagePath)) {
-			// 存在的情况下首先判断缓存中是否存在
-			Bitmap cached = null;
-			// 如果说没有大小的设置，那么就直接取缓存中的图片出来
-			if (ImageReadedSize == null) {
-				cached = cache.getBitmapFromCache(downImagePath);
-			} else {
-				// 获取这个特定宽高的缓存
-				cached = cache.getBitmapFromCache(downImagePath+ ImageReadedSize.getOptionAdditional());
-			}
-			// 如果缓存不为空
-			if (cached != null) {
-				// 直接发送过去
-				Message msg = handler
-						.obtainMessage(
-								ImageReadSDCardThreadHandler.ThreadMessageWhat.DONE.nCode,
-								cached);
-				handler.sendMessage(msg);
-				// 结束
-				return;
-			} else {
-				// 读取图片
-				Bitmap bitmap = null;
-				try {
-					bitmap = ImageReadTool.readFileBitmap(downImagePath,
-                            ImageReadedSize);
-				} catch (Exception e) {
-					// 直接发送过去
-					Message msg = handler
-							.obtainMessage(
-									ImageReadSDCardThreadHandler.ThreadMessageWhat.ERROR.nCode,
-									e);
-					handler.sendMessage(msg);
-					// 结束
-					return;
-				}
-
-				// 直接发送过去
-				Message msg = handler
-						.obtainMessage(
-								ImageReadSDCardThreadHandler.ThreadMessageWhat.DONE.nCode,
-								bitmap);
-				handler.sendMessage(msg);
-				// 保存到缓存中
-				if (ImageReadedSize == null) {
-					cache.addBitmapToCache(downImagePath, bitmap);
-				} else {
-					cache.addBitmapToCache(
-							downImagePath + ImageReadedSize.getOptionAdditional(), bitmap);
-				}
-				// 结束
-				return;
-			}
-		}
-		
-		// SD卡中不存在这张图片，那么久没办法了，只能去下载了
-		else {
-			Message msg = handler.obtainMessage(
-					ImageDownloadThreadHandler.ThreadMessageWhat.ERROR.nCode,
-					new Exception("no image in sdcard"));
+		//获取key
+		String absoluteKey= NameTool.getImageAbsoluteKey(downImagePath,ImageReadedSize);
+		//获取是否存在
+		Bitmap cached=cache.getBitmapFromCache(absoluteKey);
+		// 如果缓存不为空
+		if (cached != null) {
+			// 直接发送过去
+			Message msg = handler
+					.obtainMessage(
+							ImageReadSDCardThreadHandler.ThreadMessageWhat.DONE.nCode,
+							cached);
 			handler.sendMessage(msg);
+			// 结束
+			return;
+		}else{
+			try {
+				// 读取文件
+				Bitmap bitmap = ImageReadTool.readFileBitmap(downImagePath,ImageReadedSize);
+				//成功发送
+				Message msg = handler.obtainMessage(
+						ImageReadSDCardThreadHandler.ThreadMessageWhat.DONE.nCode,
+						bitmap);
+				//成功发送
+				handler.sendMessage(msg);
+				//添加到缓存
+				cache.addBitmapToCache(absoluteKey, bitmap);
+			} catch (Exception e) {
+				// 直接发送过去
+				Message msg = handler.obtainMessage(
+								ImageReadSDCardThreadHandler.ThreadMessageWhat.ERROR.nCode,
+								e);
+				handler.sendMessage(msg);
+				// 结束
+				return;
+			}
 		}
-
 	}
 
 }
